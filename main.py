492acabd8cc77eb7498f13ea9488bf316dc5200d
pb-coding <71174645+pb-coding@users.noreply.github.com>
2021-11-01 10:47:04 +0100

    cheapest price works now for all fuels
    added also some comments - but further will come

--- main.py
+++ main.py
@@ -5,31 +5,37 @@ import requests #library to make a webrequest
 
 def getGasStationData(website, id, supportedFuels):
     
-    thisGasStationData = [0,"blank name"]
+    #TEMPLATE CREATION
+    #create list template with fake data so that there are values existing that can be replaced in this way --> thisGasStationData[index]
+    thisGasStationData = [0,"blank name"] #ID and name of gas station
+    
+    #depending on the amount of supported fuels there need to be template data so that the data replacement progress does not error
     for fuel in supportedFuels:
-        thisGasStationData.append(100)
+        thisGasStationData.append(100) #e.g. X fuels filled with price 100
 
-    thisGasStationData[0] = id
+    #REPLACEMENT WITH REAL DATA
+    thisGasStationData[0] = id #replace template data with ID of gas Station
+    name = getGasStationName(website) #parse name from gas station
+    thisGasStationData[1] = name #replace template data with name of database
     
-    name = getGasStationName(website)
-    thisGasStationData[1] = name
+    lastIndex = 0 #lastIndex of "price-type-name" has to be set to 0 so that parsing begins with the first charachter of the HTML of clever-tanken.de
     
-    lastIndex = 0
-    
+    #iterating while parsing the HTML for "price-type-name" || price-type-name is a reliable indicator of the available fuels of that gas station
     while True:
         index = website.find("price-type-name", lastIndex)
         
-        if index == -1:
+        if index == -1: #if no "price-type-name" is found (anymore) break out of the loop
             break
 
-        for id, fuel in enumerate(supportedFuels):
+        #more comments coming soon...
+        for id, fuel in enumerate(supportedFuels): #iteration through index of list AND value needed --> used enumerate for that
             startIndex = index + 17
             endIndex = website.find("</div>", startIndex)
             checkThisFuelString = website[startIndex:endIndex]
             if checkThisFuelString == fuel:
                 fuelPrice = getFuelRelatedPrice(website, endIndex)
                 thisGasStationData[id + 2] = fuelPrice
-                print("adding: ", fuel)
+                #print("adding: ", fuel)
 
         lastIndex = index +1
 
@@ -60,13 +66,13 @@ def getGasStationPrice(website):
     return price
 
 
-def getCheapestGasStation(gasStationData):
+def getCheapestGasStation(gasStationDatabase, requestedFuelDatabaseIndex):
     current = 100
     currentName = ""
-    for gasStation in gasStationData:
-        if gasStation[1] < current:
-            current = gasStation[1]
-            currentName = gasStation[0]
+    for gasStation in gasStationDatabase:
+        if gasStation[requestedFuelDatabaseIndex] < current:
+            current = gasStation[requestedFuelDatabaseIndex]
+            currentName = gasStation[1]
     
     cheapestGasStation = [currentName, current]
     return cheapestGasStation
@@ -75,7 +81,7 @@ def getCheapestGasStation(gasStationData):
 #START-POINT
 #create empty gasStation List where info will be stored after requesting them
 base_url = "https://www.clever-tanken.de/tankstelle_details/"
-supportedFuels = ["Diesel","Super E10", "Super E5", "SuperPlus"]
+supportedFuels = ["Diesel","Super E10", "Super E5", "SuperPlus", "ARAL Ultimate Diesel"]
 gasStationsDatabase = []
 
 print("read text file where all clever-tanken gas station IDs of interest are stored..")
@@ -95,16 +101,13 @@ for id in gasStationLinks:
         print("requesting link" + link + "failed. Skipping that gas station.")
         continue  
     
-    #try:
-        
+    try:
+        thisGasStationData = getGasStationData(website, id, supportedFuels)
+        gasStationsDatabase.append(thisGasStationData)
 
-        #price = getGasStationPrice(website)
-    thisGasStationData = getGasStationData(website, id, supportedFuels)
-    gasStationsDatabase.append(thisGasStationData)
-
-    #except:
-    #    print("error - parsing gas station data from website HTML went wrong. Skipping that gas station.")
-    #    continue
+    except:
+        print("error - parsing gas station data from website HTML went wrong. Skipping that gas station.")
+        continue
     
 
 print("data collected and stored.\n")
@@ -125,14 +128,30 @@ while True:
             print("\n")
 
 
-    #TODO cheapest function needs to be adjusted to support several fuels
-    elif "cheapest":
-        cheapestGasStation = getCheapestGasStation(gasStationsDatabase)
-        if cheapestGasStation[1] == 100 or cheapestGasStation[0] == "":
-            print("error - no data")
+    elif userInput == "cheapest":
+        print("You can compare these fuels: ", supportedFuels)
+        requestedFuel = input("Please enter the type of fuel you want to compare: ")
+        try:
+            requestedFuelIndex = supportedFuels.index(requestedFuel) #Fuel Price Index in supportedFuels List
+            requestedFuelDatabaseIndex = requestedFuelIndex + 2 #Fuel Price Index in gasStationDatabase
+            validFuel = True
+
+        except:
+            validFuel = False
+            print("That fuel is not supported.")
+
+        if validFuel == True:
         
-        else:
-            print("Price:",cheapestGasStation[1], "€ - ", cheapestGasStation[0])
-    
+            cheapestGasStation = getCheapestGasStation(gasStationsDatabase, requestedFuelDatabaseIndex)
+            
+            if cheapestGasStation[1] == 100 or cheapestGasStation[0] == "":
+                print("error - no data")
+            
+            else:
+                print(supportedFuels[requestedFuelIndex],": ",cheapestGasStation[1], "€ - ", cheapestGasStation[0])
+
+    elif userInput == "quit":
+        quit()
+
     else:
         print("Unknown input. Ignoring.")
